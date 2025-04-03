c391937e265059324155b30cd13f028fdf25867d
Danillo <danillo.yamagush@gmail.com>
2025-04-03 18:15:03 -0300

    Implementation Person

--- RestWithASPNETUdemy/Controllers/PersonController.cs
+++ RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETUdemy.model;
 using RestWithASPNETUdemy.Service;
 
 namespace RestWithASPNETUdemy.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class PersonController : ControllerBase
     {
         private readonly ILogger<PersonController> _logger;
@@ -21,13 +22,35 @@ namespace RestWithASPNETUdemy.Controllers
         {
             return Ok(_personService.FindAll());
         }
-        
-        [HttpGet("id")]
+
+        [HttpGet("{id}")]
+
         public IActionResult Get(long id)
         {
             var person = _personService.FindById(id);
             if (person == null) return NotFound();
-            return Ok();
+            return Ok(person);
+        }
+        
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Create(person));
+        }
+        
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Update(person));
+        }
+        
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            return NoContent();
         }
     }
 }
